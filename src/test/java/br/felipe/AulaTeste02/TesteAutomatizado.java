62a23baf2c4cecc9307c555d5230c7a5fa2a4b07
Felipe Picinin <felipepicinin10@gmail.com>
2018-09-29 16:37:11 -0300

    Selenium enviando

--- src/test/java/br/felipe/AulaTeste02/TesteAutomatizado.java
+++ src/test/java/br/felipe/AulaTeste02/TesteAutomatizado.java
@@ -13,11 +13,19 @@ public class TesteAutomatizado {
 
 		System.setProperty("webdriver.chrome.driver", "/Users/Picinin-Vaio/Downloads/chromedriver_win32/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("http://www.google.com.br");
-		WebElement campoDeTexto = driver.findElement(By.name("q"));
-		campoDeTexto.sendKeys("faustão");
-		campoDeTexto.submit();
-		driver.close();
+		driver.get("http://192.168.4.103:8080/correntista/novo");
+		
+		
+		WebElement campoDeTexto = driver.findElement(By.name("nome"));
+		campoDeTexto.sendKeys("Felipe");
+				
+		WebElement campoDeTexto2 = driver.findElement(By.name("email"));
+		campoDeTexto2.sendKeys("felipepicinin10@gmail.com");
+				
+		WebElement botaoSalvar = driver.findElement(By.id("button1"));
+        botaoSalvar.click();
+        
+        
 	}
 
 }
